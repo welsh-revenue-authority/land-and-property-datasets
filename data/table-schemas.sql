6b627ac45c65a1b058ade5327f6f582c9160a8da
Luke Sawicki <luke.sawicki.it@gmail.com>
2022-07-13 11:53:47 +0100

    updated data model

--- data/table-schemas.sql
+++ data/table-schemas.sql
@@ -1,41 +1,41 @@
--- Table: public.land-transaction-tax-category
+-- Table: public.land_transaction_tax_category
 
-CREATE TABLE IF NOT EXISTS public."land-transaction-tax-category"
+CREATE TABLE IF NOT EXISTS public."land_transaction_tax_category"
 (
     id SERIAL PRIMARY KEY,
     name_en character varying COLLATE pg_catalog."default" NOT NULL,
 	name_cy character varying COLLATE pg_catalog."default" NOT NULL,
     description_en character varying COLLATE pg_catalog."default",
     description_cy character varying COLLATE pg_catalog."default",
-    "documentation-url_en" character varying COLLATE pg_catalog."default",
-    "documentation-url_cy" character varying COLLATE pg_catalog."default",
-    "land-transaction-tax-band" character varying COLLATE pg_catalog."default",
-    "start-date" date,
-    "end-date" date
+    "documentation_url_en" character varying COLLATE pg_catalog."default",
+    "documentation_url_cy" character varying COLLATE pg_catalog."default",
+    "land_transaction_tax_band" character varying COLLATE pg_catalog."default",
+    "start_date" date,
+    "end_date" date
 )
 
--- Table: public.land-transaction-tax-rate
+-- Table: public.land_transaction_tax_rate
 
-CREATE TABLE IF NOT EXISTS public."land-transaction-tax-rate"
+CREATE TABLE IF NOT EXISTS public."land_transaction_tax_rate"
 (
     id SERIAL PRIMARY KEY,
     description_en character varying COLLATE pg_catalog."default",
     description_cy character varying COLLATE pg_catalog."default",
-    "lower-threshold" numeric(15,2),
-    "upper-threshold" numeric(15,2),
+    "lower_threshold" numeric(15,2),
+    "upper_threshold" numeric(15,2),
     "rate" numeric(10,2),
-    "land-transaction-tax-category" character varying COLLATE pg_catalog."default",
-    "geography-id" character varying COLLATE pg_catalog."default",
+    "land_transaction_tax_category" character varying COLLATE pg_catalog."default",
+    "geography_id" character varying COLLATE pg_catalog."default",
     "organisation" character varying COLLATE pg_catalog."default",
-    "start-date" date,
-    "end-date" date
+    "start_date" date,
+    "end_date" date
 )
 
 -- Table: public.use-class-part
 
--- DROP TABLE IF EXISTS public."use-class-part";
+-- DROP TABLE IF EXISTS public."use_class_part";
 
-CREATE TABLE IF NOT EXISTS public."use-class-part"
+CREATE TABLE IF NOT EXISTS public."use_class_part"
 (
     id SERIAL PRIMARY KEY,
     "reference" character varying COLLATE pg_catalog."default",
@@ -43,28 +43,28 @@ CREATE TABLE IF NOT EXISTS public."use-class-part"
 	name_cy character varying COLLATE pg_catalog."default",
     description_en character varying COLLATE pg_catalog."default",
     description_cy character varying COLLATE pg_catalog."default",
-    "use-class-part" character varying COLLATE pg_catalog."default",
+    "use_class_part" character varying COLLATE pg_catalog."default",
     "organisation" character varying COLLATE pg_catalog."default",
-    "start-date" date,
-    "end-date" date
+    "start_date" date,
+    "end_date" date
 )
 
--- Table: public.use-class
+-- Table: public.use_class
 
--- DROP TABLE IF EXISTS public."use-class";
+-- DROP TABLE IF EXISTS public."use_class";
 
-CREATE TABLE IF NOT EXISTS public."use-class"
+CREATE TABLE IF NOT EXISTS public."use_class"
 (
     id SERIAL PRIMARY KEY,
     "reference" character varying COLLATE pg_catalog."default",
-    "use-class" character varying COLLATE pg_catalog."default",
-    "use-class-part" character varying COLLATE pg_catalog."default",
+    "use_class" character varying COLLATE pg_catalog."default",
+    "use_class_part" character varying COLLATE pg_catalog."default",
     name_en character varying COLLATE pg_catalog."default",
 	name_cy character varying COLLATE pg_catalog."default",
     description_en character varying COLLATE pg_catalog."default",
     description_cy character varying COLLATE pg_catalog."default",
-    "usage-types_en" character varying COLLATE pg_catalog."default",
-    "usage-types_cy" character varying COLLATE pg_catalog."default",
-    "start-date" date,
-    "end-date" date
+    "usage_types_en" character varying COLLATE pg_catalog."default",
+    "usage_types_cy" character varying COLLATE pg_catalog."default",
+    "start_date" date,
+    "end_date" date
 )
